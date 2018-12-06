bf5d7a0857a6f70a6688c74dae9a07db2b167634
Carrie McClanahan <cmcclana@umich.edu>
2018-12-06 07:39:43 -0500

    upload tokens

---
+++ create-keys.py
@@ -0,0 +1,12 @@
+import redis
+import os
+import uuid
+
+r = redis.from_url(os.environ.get("REDIS_URL"))
+csv = open('./tokens.csv', 'w')
+csv.write('website_url\n')
+for i in range(500):
+  token = uuid.uuid4().hex
+  csv.write(f'{token}\n')
+  r.set(token, '{ }')
+csv.close()
